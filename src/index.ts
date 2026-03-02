8ddb282247019d13f2170fc7ea12161dc73e4dc5
norovsuren15 <e.norvoo@gmail.com>
2026-03-02 12:44:41 +0800

    backend setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import type { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("This is get req");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
